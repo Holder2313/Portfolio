d0708d0db48ad560244686722c1a67ba3a6e5c67
sebastien Perrot <spholder4@gmail.com>
2024-06-17 23:29:34 +0200

    fix nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         domains: ['v1jstue0cv3dqylt.public.blob.vercel-storage.com'],
-    }
+    },
+    env: {
+        DATABASE_URL: process.env.DATABASE_URL,
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        GITHUB_ID: process.env.GITHUB_ID,
+        GITHUB_SECRET: process.env.GITHUB_SECRET,
+        EMAIL_USER: process.env.EMAIL_USER,
+        BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN,
+    },
 };
 
 export default nextConfig;
